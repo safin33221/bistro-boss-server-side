31d1f898ef36864ad989941b1a491b066ff62408
safin33221 <saifn33221@gmail.com>
2025-01-03 00:21:57 +0600

    completed to menu  item form db

--- index.js
+++ index.js
@@ -25,13 +25,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const dataBase = client.db('bistroBoss')
+        const menuCollection = dataBase.collection('menu')
+
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result )
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
