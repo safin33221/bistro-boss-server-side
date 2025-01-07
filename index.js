e5cfd27c4d209b994b6a62486c996b6cf7eb765c
safin33221 <saifn33221@gmail.com>
2025-01-08 00:59:37 +0600

    completed to delted item by admin and sucured it

--- index.js
+++ index.js
@@ -161,6 +161,13 @@ async function run() {
             const result = await cartsCollection.deleteOne(query)
             res.send(result)
         })
+        //deleted menu item by admin and it's secure
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
