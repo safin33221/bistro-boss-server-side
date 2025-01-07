0fe012985534c1d7ed580e87150d8b8abb082fbe
safin33221 <saifn33221@gmail.com>
2025-01-07 23:41:25 +0600

    post menu data completed in menu collection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const jwt = require('jsonwebtoken')
 const cors = require('cors');
 const port = process.env.PORT || 5050
 require('dotenv').config()
@@ -12,6 +13,12 @@ app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blz8y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// 1st steps create token
+// 2nd get token from cliend side
+//3rd middle for very token
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,10 +40,62 @@ async function run() {
         const menuCollection = client.db('bistroBoss').collection('menu')
         const cartsCollection = client.db('bistroBoss').collection('carts')
 
+        //JWT related api's
+        //3rd middle for very token
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req?.headers?.authorization) {
+                return res.status(401).send({ message: 'forbidden access' })
+            }
+            const token = req?.headers?.authorization.split(' ')[1]
+            jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded
+                next()
+            })
+        }
+
+        //verify admin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const isAdmin = user?.role === "admin"
+            if (!isAdmin) {
+                res.send(403).send({ message: 'forbidden error' })
+            }
+            next()
+        }
+
+        // 1st steps create token
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '4h' })
+            res.send({ token })
+        })
+
         //get user data form usercollection
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            // console.log(req.headers); // 2nd get token from cliend side
             const result = await usersCollection.find().toArray()
             res.send(result)
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            console.log(email, req.decoded.email);
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden  Access' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
         })
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
@@ -68,23 +127,28 @@ async function run() {
             const result = await cartsCollection.insertOne(food)
             res.send(result)
         })
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const menuData = req.body;
+            const result = await menuCollection.insertOne(menuData)
+            res.send(result)
+        })
 
         //user user role
-        app.patch('/user/admin/:id', async (req, res) => {
+        app.patch('/user/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set: {
-                    role:'admin'
+                    role: 'admin'
                 }
             }
-            const result = await usersCollection.updateOne(filter,updateDoc)
+            const result = await usersCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
 
         //deleted user form usecollection
-        app.delete('/users/:id', async (req, res) => {
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
